468b81c7a413dc50076a06aac15e94068f52ff1e
Thayne McCombs <astrothayne@gmail.com>
2022-06-08 00:31:46 -0600

    Re-implement using hyper 0.14

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,62 @@
 extern crate hyper;
 
-use std::io::prelude::*;
-use std::io::{stdout, Result};
+use std::convert::Infallible;
+use std::io::{stdout, Write};
+use std::net::SocketAddr;
 
-use hyper::header::Headers;
-use hyper::server::{Server, Request, Response};
+use http::header::HeaderMap;
+use hyper::body::{HttpBody, Sender};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
 
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let make_service = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(echo)) });
+    let addr: SocketAddr = ([0, 0, 0, 0], 3000).into();
+    let server = Server::bind(&addr).serve(make_service);
 
-
-fn main() {
-  Server::http("0.0.0.0:3000").and_then(|server| {
-    server.handle(echo_handler)
-  }).expect("Unable to listen on port 3000");
-}
-
-fn echo_handler(req: Request, resp: Response) {
-  match do_echo(req, resp) {
-    Ok(_) => {},
-    Err(e) => {
-      println!("Error: {}\n", e);
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    } else {
+        println!("Listening on port 3000");
     }
-  }
 }
 
-fn do_echo(mut req: Request, resp: Response) -> Result<()> {
-  // print the request line
-  println!("{} {} {}\n", req.method, req.uri, req.version);
-  print_headers(&req.headers);
-  print!("\n");
+async fn echo(mut req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    // print the request line
+    println!("{} {} {:?}\n", req.method(), req.uri(), req.version());
+    print_headers(&req.headers());
+    print!("\n");
 
-  let mut buffer: [u8; 1024] = [0; 1024];
-  let mut response = try!(resp.start());
-  let mut out = stdout();
-  while let Ok(count) = req.read(&mut buffer) {
-    if count == 0 {
-      break;
+    let (sender, body) = Body::channel();
+
+    tokio::spawn(async move {
+        if let Err(err) = do_echo(req.body_mut(), sender).await {
+            eprintln!("Error echoing content: {}", err);
+        }
+    });
+
+    Ok(Response::new(body))
+}
+
+async fn do_echo(req: &mut Body, mut resp: Sender) -> Result<(), hyper::Error> {
+    let mut out = stdout();
+    while let Some(data) = req.data().await {
+        let data = data?;
+        let _ = out.write_all(&data);
+        resp.send_data(data.clone()).await?;
     }
-    let chunk = &buffer[0..count];
-    try!(response.write_all(chunk));
-    try!(out.write_all(chunk));
-  }
-  print!("\n\n");
-  try!(out.flush());
-  response.end()
+    write!(out, "\n\n").unwrap();
+    out.flush().unwrap();
+    if let Ok(Some(ref trailers)) = req.trailers().await {
+        println!("Trailers:");
+        print_headers(trailers);
+    }
+    Ok(())
 }
 
-fn print_headers(headers: &Headers) {
-  for header in headers.iter() {
-    println!("{}: {}", header.name(), header.value_string());
-  }
+fn print_headers(headers: &HeaderMap) {
+    for (name, value) in headers.iter() {
+        println!("{}: {}", name, String::from_utf8_lossy(value.as_bytes()));
+    }
 }
